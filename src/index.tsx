dab56d3145c7b24c7f2831130fea4bb6b5b471fd
Lucas Augusto <lucas.augusto5061@gmail.com>
2020-10-27 17:39:31 -0300

    settings global style

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux';
 import store from 'store';
 import App from './app/App';
 
+import GlobalStyle from './assets/styles/globalStyle';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
