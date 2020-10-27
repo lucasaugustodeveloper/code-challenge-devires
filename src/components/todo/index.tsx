7ad15c6aacb4ba8aa84ea52f5c8e7c4c54783c68
Lucas Augusto <lucas.augusto5061@gmail.com>
2020-10-27 17:38:43 -0300

    add component for render todo list

---
+++ src/components/todo/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TodoList = () => <div>todo list</div>;
+
+export default TodoList;
---
+++ src/components/todo/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { Container as Ctn } from '../../assets/styles/globalStyle';
+
+export const Container = styled(Ctn)`
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const Button = styled.button``;
