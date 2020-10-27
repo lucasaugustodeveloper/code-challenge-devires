920a487cca2cb4b3351cbf8fc36a0b3c7593def8
Lucas Augusto <lucas.augusto5061@gmail.com>
2020-10-27 17:40:12 -0300

    started mount page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
+import * as S from '../assets/styles/globalStyle';
+
+import Header from '../components/header';
+import InputSearch from '../components/input';
+import TodoList from '../components/todo';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Code Challenge!</h1>
-    </div>
+    <S.Container>
+      <Header>Todo List</Header>
+
+      <S.Container>
+        <InputSearch
+          type="text"
+          name="search"
+          value="value"
+          placeholder="enter your task"
+          id="search"
+        />
+      </S.Container>
+
+      <S.Main>
+        <TodoList />
+      </S.Main>
+    </S.Container>
   );
 }
 
