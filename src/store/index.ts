12bdbf22eedfe85df3179aa2c96c9d129a05dcf5
Lucas Augusto <lucas.augusto5061@gmail.com>
2020-10-31 17:51:15 -0300

    add and settings redux-thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Action } from '@reduxjs/toolkit';
+import { ThunkAction } from 'redux-thunk';
 
 import reducers from './ducks';
 
-export type RootState = ReturnType<typeof reducers>;
-
 const store = configureStore({ reducer: reducers });
+
+export type RootState = ReturnType<typeof reducers>;
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 
 export default store;
